39d99e8641677af2f5b3903c73013ba3b9a968af
Jassmine Tatum <jassmine.tatum@gmail.com>
2020-11-03 22:01:11 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+    def initialize(name,breed="Mutt")
+        @name = name 
+        @breed = breed 
+        if @breed == nil
+            puts "Mutt"
+
+        end 
+    
+
+    end 
+
+end 
+
+fido = Dog.new("Fido","Pug")
